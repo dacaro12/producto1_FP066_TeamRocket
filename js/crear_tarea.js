fa9c8f019c4deccbdc1e8c2b27bd2b96277febcb
avillo12 <115428934+annavm12@users.noreply.github.com>
2023-03-25 16:46:43 +0100

    crear_tarea ok

--- js/crear_tarea.js
+++ js/crear_tarea.js
@@ -1,62 +1,69 @@
 function crearTarea() {
 
-    const form = document.querySelector('form');
-    if (!form.checkValidity()) {
-        const errorMessage = document.querySelector('.error-message');
-        errorMessage.style.display = 'block';
-        return; // salimos de la función si el formulario no es válido
+    // obtener valores de los inputs del formulario
+    const nombre = document.getElementById("tarea-input").value;
+    const horaInicio = document.getElementById("time-input").value;
+    const horaFinal = document.getElementById("time-input").value;
+    const descripcion = document.getElementById("descripcion-input").value;
+    const colaboradores = document.getElementById("colaboradores-input").value;
+    const prioridad = document.getElementById("prioridad").value;
+  
+    // validar que se hayan llenado todos los campos
+    if (!nombre || !horaInicio || !horaFinal || !descripcion || !colaboradores || !prioridad) {
+      const errorDiv = document.querySelector(".error-message");
+      errorDiv.style.display = "block";
+      return;
     }
-
-
-    // Obtener valores de los campos del formulario
-    var nombreTarea = document.getElementById("tarea-input").value;
-    var horaInicio = document.getElementById("time-input").value;
-    var horaFinal = document.getElementById("time-input").value;
-    var descripcion = document.getElementById("descripcion-input").value;
-    var colaboradores = document.getElementById("colaboradores-input").value;
-    var prioridad = document.getElementById("prioridad").value;
-
-    if (!nombreTarea || !horaInicio || !horaFinal || !descripcion || !colaboradores || !prioridad) {
-        return; // salimos de la función si algún campo requerido está vacío
-    }
-    //crear card
-    let card = document.createElement("div");
-    card.className = "card";
+  
+    // crear elemento card
+    const card = document.createElement("div");
+    card.classList.add("card");
     card.style.backgroundColor = prioridad;
+    card.style.width = "200px";
+    card.style.height = "155px";
   
-    // Crear tarjeta con los valores del formulario
-    var contenedor = document.getElementById("container-tareas");
-    var nuevaTarjeta = document.createElement("div");
-    nuevaTarjeta.classList.add("card");
-    nuevaTarjeta.style.backgroundColor = prioridad;
-
-    // Crear el cuerpo de la tarjeta
-    var cuerpoTarjeta = document.createElement("div");
-    cuerpoTarjeta.classList.add("card-body");
-    cuerpoTarjeta.style.backgroundColor = prioridad;
-
-    // Agregar los elementos al cuerpo de la tarjeta
-    cuerpoTarjeta.innerHTML = `
-    <h6 class="card-title">${nombreTarea}</h6>
-    <p class="card-text">${descripcion}</p>
-    <ul class="list-group list-group-flush" style="background-color:${prioridad};">
-        <li class="list-group-item"><strong>Inicio:</strong> ${horaInicio}</li>
-        <li class="list-group-item"><strong>Fin:</strong> ${horaFinal}</li>
-        <li class="list-group-item"><strong>Colaboradores:</strong> ${colaboradores}</li>
-    </ul>
-    `;
-
-    // Agregar el cuerpo de la tarjeta a la tarjeta
-    nuevaTarjeta.appendChild(cuerpoTarjeta);
-
-    // Agregar la nueva tarjeta al contenedor
-    contenedor.appendChild(nuevaTarjeta);
-
+    // crear elemento card-body
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
   
-    // Cerrar el modal
+    // crear elementos para mostrar los datos de la tarea
+    const nombreTarea = document.createElement("h5");
+    nombreTarea.classList.add("card-title");
+    nombreTarea.textContent = nombre;
+  
+    const horaInicioTarea = document.createElement("p");
+    horaInicioTarea.classList.add("card-text");
+    horaInicioTarea.textContent = "Hora de inicio: " + horaInicio;
+  
+    const horaFinalTarea = document.createElement("p");
+    horaFinalTarea.classList.add("card-text");
+    horaFinalTarea.textContent = "Hora de finalización: " + horaFinal;
+  
+    const descripcionTarea = document.createElement("p");
+    descripcionTarea.classList.add("card-text");
+    descripcionTarea.textContent = "Descripción: " + descripcion;
+  
+    const colaboradoresTarea = document.createElement("p");
+    colaboradoresTarea.classList.add("card-text");
+    colaboradoresTarea.textContent = "Colaboradores: " + colaboradores;
+  
+    // añadir elementos a la card
+    cardBody.appendChild(nombreTarea);
+    cardBody.appendChild(horaInicioTarea);
+    cardBody.appendChild(horaFinalTarea);
+    cardBody.appendChild(descripcionTarea);
+    cardBody.appendChild(colaboradoresTarea);
+  
+    card.appendChild(cardBody);
+  
+    // añadir card al container de tareas
+    const container = document.getElementById("container-tareas");
+    container.appendChild(card);
+  
+    // cerrar modal
     $('#modal-tarea').modal('hide');
   
-    // Limpiar los campos del formulario
+    // limpiar valores del formulario
     document.getElementById("formulario").reset();
   }
   
